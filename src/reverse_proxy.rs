99974821e556b45f13674c6dd751474dd8460a13
asyrafduyshart <me@asyraf.id>
2024-03-21 04:39:30 +0000

    - fix bugs

--- src/reverse_proxy.rs
+++ src/reverse_proxy.rs
@@ -10,13 +10,14 @@ pub async fn forward(
 ) -> Result<HttpResponse, Error> {
     let client = app_data.client.clone();
     let config = app_data.config.clone();
-    let mut new_url = Url::parse(config.proxy.as_str()).unwrap();
+
+    let mut new_url = match Url::parse(config.proxy.as_str()) {
+        Ok(url) => url,
+        Err(err) => return Err(error::ErrorInternalServerError(err)),
+    };
+
     new_url.set_path(req.uri().path());
     new_url.set_query(req.uri().query());
-
-    // TODO: This forwarded implementation is incomplete as it only handles the inofficial
-    // X-Forwarded-For header but not the official Forwarded one.
-    // make client request support tls
 
     let forwarded_req = client.request_from(new_url.as_str(), req.head());
 
@@ -26,11 +27,6 @@ pub async fn forward(
         .map_err(error::ErrorInternalServerError)?;
 
     let mut client_resp = HttpResponse::build(res.status());
-    // Remove `Connection` as per
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection#Directives
-    for (header_name, header_value) in res.headers().iter().filter(|(h, _)| *h != "connection") {
-        client_resp.append_header((header_name.clone(), header_value.clone()));
-    }
 
     Ok(client_resp.streaming(res))
 }
